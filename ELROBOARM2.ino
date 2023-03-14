db72d61dfbb6f7594952dd8a17c9a6d52af250ce
RagingBull6287 <123290856+RagingBull6287@users.noreply.github.com>
2023-03-14 21:27:58 +0530

    Update ELROBOARM2.ino

--- ELROBOARM2.ino
+++ ELROBOARM2.ino
@@ -3,117 +3,94 @@
 #include "Constants.h"
 #include "StepperControl.h"
 
-enum Mode
-{
-    adjustingJoint1,
-    adjustingJoint2,
-    adjustingJoint3,
-    adjustingJoint4,
-};
-
-Mode currentMode = adjustingJoint1;
 
 Rotarydrive Rotary1(ROTARY_ENC_1_A, ROTARY_ENC_1_B);
 StepperControl MOTOR1(STEPPER1_DIR_PIN, STEPPER1_STEP_PIN);
 StepperControl MOTOR2(STEPPER2_DIR_PIN, STEPPER2_STEP_PIN);
 StepperControl MOTOR3(STEPPER3_DIR_PIN, STEPPER3_STEP_PIN);
 StepperControl MOTOR4(STEPPER4_DIR_PIN, STEPPER4_STEP_PIN);
+StepperControl MOTOR5(STEPPER5_DIR_PIN, STEPPER5_STEP_PIN);
+struct Button {
+    uint32_t numberKeyPresses;
+    bool pressed;
+};
+Button button1 = {0, false};
 
-int buttonCoolDowncounterer = 0;
-hw_timer_t *timer = NULL;
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-volatile SemaphoreHandle_t timerSemaphore;
+//variables to keep track of the timing of recent interrupts
+unsigned long button_time = 0;  
+unsigned long last_button_time = 0; 
 
-void handleModeChange(Mode newMode)
+
+
+void IRAM_ATTR isr() {
+    button_time = millis();
+if (button_time - last_button_time > 250)
 {
-    if (buttonCoolDowncounterer < BUTTON_COOLDOWN_CYCLES)
-    {
-        buttonCoolDowncounterer++;
-    }
-    if (digitalRead(BUTTON_PIN) && buttonCoolDowncounterer >= BUTTON_COOLDOWN_CYCLES)
-    {
-        buttonCoolDowncounterer = 0;
-        currentMode = newMode;
-    }
+  {
+       if(button1.numberKeyPresses == 4)
+       {
+         button1.numberKeyPresses = 0;
+       }
+       else
+        button1.numberKeyPresses++;
+  }
+        button1.pressed = true;
+       last_button_time = button_time;
+}
 }
 
-void IRAM_ATTR onTimer()
-{
-    digitalWrite(EXECUTING_ISR_CODE, HIGH);
-
-    switch (currentMode)
-    {
-    case adjustingJoint1:
-        {
-          Rotary1.update();
-          MOTOR1.RUN(Rotary1.counter);
-        
-          handleModeChange(adjustingJoint2);
-        }
-        break;
-    case adjustingJoint2:
-        {
-          Rotary1.update();
-          MOTOR2.RUN(Rotary1.counter);
-          
-          handleModeChange(adjustingJoint3);
-                
-        }
-         break;
-    case adjustingJoint3:
-        {        
-          Rotary1.update();
-          MOTOR3.RUN(Rotary1.counter);
-          
-          handleModeChange(adjustingJoint4);
-        }
-        break;
-    case adjustingJoint4:
-        {
-           Rotary1.update();
-           MOTOR4.RUN(Rotary1.counter);
-          
-           
-        }
-        break;
-    
-    default:
-        break;
-    }
-
-    xSemaphoreGiveFromISR(timerSemaphore, NULL);
-    digitalWrite(EXECUTING_ISR_CODE, LOW);
-}
 
 void setup()
 {
     Serial.begin(115200);
-     pinMode(EXECUTING_ISR_CODE, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT);
-
-    timerSemaphore = xSemaphoreCreateBinary();
-
-    timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(timer, &onTimer, true);
-    // onTimer gets called every 100uS.
-    timerAlarmWrite(timer, 100, true);
-    timerAlarmEnable(timer);
-
-
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    attachInterrupt(BUTTON_PIN, isr, FALLING);
 }
 
 void loop()
 {
-        if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE)
-    {
-      
+ if (button1.pressed) {
+        Serial.printf("Button has been pressed %u times\n", button1.numberKeyPresses);
+        button1.pressed = false;
 
-        portENTER_CRITICAL(&timerMux);
-      
-        portEXIT_CRITICAL(&timerMux);
-
-        //Serial.print("MODE: ");
-        //Serial.print(currentMode);
-        delay(10);
     }
+ 
+
+  if(button1.numberKeyPresses == 0)
+        {
+          Rotary1.update();
+          MOTOR1.RUN(Rotary1.counter);
+          Serial.print("Adjusting Joint 1");
+        }
+        
+  else if(button1.numberKeyPresses == 1 )
+        {
+          Rotary1.update();
+          MOTOR2.RUN(Rotary1.counter);
+          Serial.print("Adjusting Joint 2");
+                
+        }
+         
+   else if(button1.numberKeyPresses == 2 )
+        {        
+          Rotary1.update();
+          MOTOR3.RUN(Rotary1.counter);
+          Serial.print("Adjusting Joint 3");
+        }
+        
+    else if(button1.numberKeyPresses == 3)
+        {
+           Rotary1.update();
+           MOTOR4.RUN(Rotary1.counter);
+          Serial.print("Adjusting Joint 4");
+           
+        }
+    else if(button1.numberKeyPresses == 4)
+        {
+           Rotary1.update();
+           MOTOR5.RUN(Rotary1.counter);
+          Serial.print("Adjusting Base");
+        }
+    else
+         Serial.println("error");
 }
