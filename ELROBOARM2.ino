6e9217e230a208e70e106ca5046dc2b185531c9b
RagingBull6287 <123290856+RagingBull6287@users.noreply.github.com>
2023-03-14 13:48:30 +0530

    Add files via upload

--- ELROBOARM2.ino
+++ ELROBOARM2.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "Rotarydrive.h"
 #include "Constants.h"
+#include "StepperControl.h"
 
 enum Mode
 {
@@ -8,13 +9,15 @@ enum Mode
     adjustingJoint2,
     adjustingJoint3,
     adjustingJoint4,
-    doingControlledMovements,
-    error
 };
 
 Mode currentMode = adjustingJoint1;
 
 Rotarydrive Rotary1(ROTARY_ENC_1_A, ROTARY_ENC_1_B);
+StepperControl MOTOR1(STEPPER1_DIR_PIN, STEPPER1_STEP_PIN);
+StepperControl MOTOR2(STEPPER2_DIR_PIN, STEPPER2_STEP_PIN);
+StepperControl MOTOR3(STEPPER3_DIR_PIN, STEPPER3_STEP_PIN);
+StepperControl MOTOR4(STEPPER4_DIR_PIN, STEPPER4_STEP_PIN);
 
 int buttonCoolDowncounterer = 0;
 hw_timer_t *timer = NULL;
@@ -41,40 +44,43 @@ void IRAM_ATTR onTimer()
     switch (currentMode)
     {
     case adjustingJoint1:
-        Rotary1.update();
-        digitalWrite(STEPPER1_DIR_PIN, Rotary1.currentRot);
-        digitalWrite(STEPPER1_STEP_PIN, Rotary1.counter % 2);
-
-        handleModeChange(adjustingJoint2);
+        {
+          Rotary1.update();
+          MOTOR1.RUN(Rotary1.counter);
+        
+          handleModeChange(adjustingJoint2);
+        }
         break;
     case adjustingJoint2:
-        Rotary1.update();
-        digitalWrite(STEPPER2_DIR_PIN, Rotary1.currentRot);
-        digitalWrite(STEPPER2_STEP_PIN, Rotary1.counter % 2);
-
-        handleModeChange(adjustingJoint3);
-        break;
+        {
+          Rotary1.update();
+          MOTOR2.RUN(Rotary1.counter);
+          
+          handleModeChange(adjustingJoint3);
+                
+        }
+         break;
     case adjustingJoint3:
-        Rotary1.update();
-        digitalWrite(STEPPER3_DIR_PIN, Rotary1.currentRot);
-        digitalWrite(STEPPER3_STEP_PIN, Rotary1.counter % 2);
-
-        handleModeChange(adjustingJoint4);
+        {        
+          Rotary1.update();
+          MOTOR3.RUN(Rotary1.counter);
+          
+          handleModeChange(adjustingJoint4);
+        }
         break;
     case adjustingJoint4:
-        Rotary1.update();
-        digitalWrite(STEPPER4_DIR_PIN, Rotary1.currentRot);
-        digitalWrite(STEPPER4_STEP_PIN, Rotary1.counter % 2);
-
-        handleModeChange(doingControlledMovements);
+        {
+           Rotary1.update();
+           MOTOR4.RUN(Rotary1.counter);
+          
+           
+        }
         break;
-    case doingControlledMovements:
-        portENTER_CRITICAL_ISR(&timerMux);
-        portEXIT_CRITICAL_ISR(&timerMux);
-        break;
+    
     default:
         break;
     }
+
     xSemaphoreGiveFromISR(timerSemaphore, NULL);
     digitalWrite(EXECUTING_ISR_CODE, LOW);
 }
@@ -82,30 +88,32 @@ void IRAM_ATTR onTimer()
 void setup()
 {
     Serial.begin(115200);
-
-    pinMode(EXECUTING_ISR_CODE, OUTPUT);
+     pinMode(EXECUTING_ISR_CODE, OUTPUT);
     pinMode(BUTTON_PIN, INPUT);
 
     timerSemaphore = xSemaphoreCreateBinary();
-    // Set 80 divider for prescaler
+
     timer = timerBegin(0, 80, true);
     timerAttachInterrupt(timer, &onTimer, true);
     // onTimer gets called every 100uS.
     timerAlarmWrite(timer, 100, true);
     timerAlarmEnable(timer);
+
+
 }
 
 void loop()
 {
-    if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE)
+        if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE)
     {
-        
+      
+
         portENTER_CRITICAL(&timerMux);
+      
         portEXIT_CRITICAL(&timerMux);
-        Serial.print(currentMode);
-        //Serial.print("pos: ");
-        //Serial.println(pos);
+
+        //Serial.print("MODE: ");
+        //Serial.print(currentMode);
         delay(10);
     }
 }
-
